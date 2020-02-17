21161a83e9fd395eda1789581bd05922a54ac539
liangjun <420333870@qq.com>
2020-02-17 13:40:10 +0800

    initial the code

---
+++ taxi-service-demo/src/main/java/com/zacx/taxi/TaxiServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zacx.taxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiServiceDemoApplication.class, args);
+	}
+
+}
---
+++ taxi-service-demo/src/test/java/com/zacx/taxi/TaxiServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacx.taxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
